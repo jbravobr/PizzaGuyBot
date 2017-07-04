fb038accda5e9fc9b71d9b029daae290f2009254
Rodrigo Amaro <jbravo.br@gmail.com>
2017-07-04 05:59:08 -0300

    Atualização dos pacotes para funcionamento correto

--- PizzaGuyChaBot/Dialogs/RootDialog.cs
+++ PizzaGuyChaBot/Dialogs/RootDialog.cs
@@ -5,10 +5,11 @@ using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace PizzaGuyChaBot.Dialogs
 {
-    [LuisModel("cfc6d9c1-49ea-4474-8d79-eacf5327bfcb", "e373378781f5469d9cd828be194966cc")]
+    [LuisModel("cfc6d9c1-49ea-4474-8d79-eacf5327bfcb", "f2b4cf14f7c74165b6cb8784b48600a4")]
     [Serializable]
     public class RootDialog : LuisDialog<object>
     {
@@ -38,29 +39,31 @@ namespace PizzaGuyChaBot.Dialogs
         [LuisIntent("Order")]
         public async Task Order(IDialogContext context, LuisResult result)
         {
-			await context.PostAsync($"Tá bom, agora preciso saber então qual o sabor da pizza..");
-			context.Wait(MessageReceived);
+            await context.PostAsync($"Tá bom, agora preciso saber então qual o sabor da pizza..");
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Payment")]
         public async Task Payment(IDialogContext context, LuisResult result)
         {
-			await context.PostAsync($"Ótimo, então vamos confirmar o seu pedido!");
-			context.Wait(MessageReceived);
+            await context.PostAsync($"Ótimo, então vamos confirmar o seu pedido!");
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("PickFlavour")]
-		public async Task PickFlavour(IDialogContext context, LuisResult result)
-		{
+        public async Task PickFlavour(IDialogContext context, LuisResult result)
+        {
             var sabores = result.Entities.Select(x => x.Entity).Aggregate((f, s) => $"{f},{s}");
 
-            IMessageActivity message = Activity.CreateMessageActivity();
+            var message = context.MakeMessage();
             message.Text = $"Tá bom anotei aqui então os sabores. Agora me diz qual vai ser a forma de pagamento?";
             message.Locale = "pt-br";
+            message.ReplyToId = context.Activity.Id;
             message.Attachments.Add(new Attachment { ContentType = "text/plain", Name = "Sabores", Content = sabores });
 
             await context.PostAsync(message);
-			context.Wait(MessageReceived);
-		}
+            //await context.PostAsync($"Está ótimo, agora me diz como você vai pagar?");
+            context.Wait(MessageReceived);
+        }
     }
 }
